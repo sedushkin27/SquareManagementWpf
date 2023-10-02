9d69d63f72108a08f3e2028d5dd31d965ec12632
Vadim <sedushkin27@gmail.com>
2023-10-02 09:42:36 +0300

    created programmes

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,95 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        private const string SaveFiveName = "squarestate.xml";
+        private SquareState squareState;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            squareState = LoadSquareSatate();
+
+            if (squareState != null ) { square.Margin = new Thickness(squareState.Left, squareState.Top, 0, 0); }
+            else { squareState = new SquareState(); }
+
+            KeyDown += MainWindow_KeyDown;
+            Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            double step = 10;
+
+            switch (e.Key)
+            {
+                case Key.A:
+                    square.Margin = new Thickness(square.Margin.Left - step, square.Margin.Top, 0, 0);
+                    break;
+                case Key.D:
+                    square.Margin = new Thickness(square.Margin.Left + step, square.Margin.Top, 0, 0);
+                    break;
+                case Key.W:
+                    square.Margin = new Thickness(square.Margin.Left, square.Margin.Top - step, 0, 0);
+                    break;
+                case Key.S:
+                    square.Margin = new Thickness(square.Margin.Left, square.Margin.Top + step, 0, 0);
+                    break;
+            }
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            squareState.Left = square.Margin.Left;
+            squareState.Top = square.Margin.Top;
+
+            SaveSquareState(squareState);
+        }
+
+        private void SaveSquareState(SquareState state) 
+        {
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(SquareState));
+
+                using (StreamWriter writer = new StreamWriter(SaveFiveName))
+                {
+                    serializer.Serialize(writer, state);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failure to maintain the status: {ex.Message}", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private SquareState LoadSquareSatate()
+        {
+            if (File.Exists(SaveFiveName))
+            {
+                try
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(SquareState));
+
+                    using (StreamReader reader = new StreamReader(SaveFiveName))
+                    {
+                        return (SquareState)serializer.Deserialize(reader);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Mistake of the status upload: {ex.Message}", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+            return null;
         }
     }
 }
---
+++ WpfApp1/SquareState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    [Serializable]
+    public class SquareState
+    {
+        public double Left { get; set; }
+        public double Top { get; set; }
+    }
+}
